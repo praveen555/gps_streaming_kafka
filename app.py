fe14dde4b62766f42c259fcfb911193832526091
praveen555 <mvsprvn@gmail.com>
2023-09-28 23:05:26 -0400

    install docker image

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 def write_to_text_file(x):
     # print("Writing to file")
     # print(x)
-    file_name = os.path.join(os.getcwd(),"logs",f"log_{current_date}.txt")
+    file_name = os.path.join(os.getcwd(),"gps_logs",f"log_{current_date}.txt")
     with file_lock:
         if not os.path.exists(file_name):
             open(file_name,"w").close()
